8a96f8e7f30be14cb56122d78e5344073e3ccf60
twheelertech <tony@wheelertechconsulting.com>
2019-09-14 23:05:51 -0500

    finished ai

--- half humanoid.py
+++ half humanoid.py
@@ -97,54 +97,122 @@ class Critic(nn.Module):
     
     #Building the whole training process into a class
     
-    class TD3(object):
+class TD3(object):
         
-        def __init__(self, state_dim, action_dim, max_action):
-            self.actor = Actor(state_dim, action_dim, max_action).to(device)
-            self.actor_target = Actor(state_dim, action_dim, max_action).to(device)
-            self.actor_target.load_state_dict(self.actor.state_dict())
-            self.actor_optimizer = torch.optim.Adam(self.actor.parameters())
-            self.critic = Critic(state_dim, action_dim).to(device)
-            self.critic_target = Critic(state_dim, action_dim).to(device)
-            self.critic_target.load_state_dict(self.critic.state_dict())
-            self.critic_optimizer = torch.optim.Adam(self.critic.parameters())
-            self.max_action = max_action
+    def __init__(self, state_dim, action_dim, max_action):
+        self.actor = Actor(state_dim, action_dim, max_action).to(device)
+        self.actor_target = Actor(state_dim, action_dim, max_action).to(device)
+        self.actor_target.load_state_dict(self.actor.state_dict())
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters())
+        self.critic = Critic(state_dim, action_dim).to(device)
+        self.critic_target = Critic(state_dim, action_dim).to(device)
+        self.critic_target.load_state_dict(self.critic.state_dict())
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters())
+        self.max_action = max_action
             
-        def select_action(self, state):
-            state = torch.Tensor(state.reshape(1, -1)).to(device)
-            return self.actor(state).cpu().data.numpy().flatten()
+    def select_action(self, state):
+        state = torch.Tensor(state.reshape(1, -1)).to(device)
+        return self.actor(state).cpu().data.numpy().flatten()
         
-        def train(self, replay_buffer, iterations, batch_size=100, discount=0.99, tau=0.005, policy_noise=0.2, noise_clip=0.5, policy_freq=2):
-            for it in range(iterations):
+    def train(self, replay_buffer, iterations, batch_size=100, discount=0.99, tau=0.005, policy_noise=0.2, noise_clip=0.5, policy_freq=2):
+        for it in range(iterations):
                 
-                #Stap 4 Sample a batch of transitions (s, s', a, r) from the memory
-                batch_states, batch_next_states, batch_actions, batch_rewards, batch_dones = replay_buffer.sample(batch_size)
-                state = torch.Tensor(batch_states).to(device)
-                action = torch.Tensor(batch_actions).to(device)
-                reward = torch.Tensor(batch_rewards).to(device)
-                done = torch.Tensor(batch_dones).to(device)
+            #Stap 4 Sample a batch of transitions (s, s', a, r) from the memory
+            batch_states, batch_next_states, batch_actions, batch_rewards, batch_dones = replay_buffer.sample(batch_size)
+            state = torch.Tensor(batch_states).to(device)
+            action = torch.Tensor(batch_actions).to(device)
+            reward = torch.Tensor(batch_rewards).to(device)
+            done = torch.Tensor(batch_dones).to(device)
                 
-                #Step 5 From the next state s', the actor target plays the next action a'
-                next_action = self.actor_target(next_state)
+            #Step 5 From the next state s', the actor target plays the next action a'
+            next_action = self.actor_target(next_state)
                 
-                #step 6: We add Gaussian noise to this next action a' and we clamp it in a range of values supported by the environment
-                noise = torch.Tensor(batch_actions).data.normal_(0, policy_noise).to(device)
-                noise = noise.clamp(-noise_clip, noise_clip)
-                next_action = (next_action + noise).clamp(-self.max_action, self.max_action)
+            #step 6: We add Gaussian noise to this next action a' and we clamp it in a range of values supported by the environment
+            noise = torch.Tensor(batch_actions).data.normal_(0, policy_noise).to(device)
+            noise = noise.clamp(-noise_clip, noise_clip)
+            next_action = (next_action + noise).clamp(-self.max_action, self.max_action)
                 
-                #step 7 the two critic targets take the couple (s' a') as input and reeturn two Q-values Qt1(s’,a’) and Qt2(s’,a’) as outputs
-                target_Q1, target_Q2 = self.critic_target(next_state, next_action)
+            #step 7 the two critic targets take the couple (s' a') as input and reeturn two Q-values Qt1(s’,a’) and Qt2(s’,a’) as outputs
+            target_Q1, target_Q2 = self.critic_target(next_state, next_action)
                 
-                #step 8: keep the minimum of these two Q-values: min(Qt1, Qt2)
-                target_Q = torch.min(target_Q1, target_Q2)
+            #step 8: keep the minimum of these two Q-values: min(Qt1, Qt2)
+            target_Q = torch.min(target_Q1, target_Q2)
                 
-                # get the final target of the two Critic models, which is: Qt = r + γ * min(Qt1, Qt2), where γ is the discount factor
-                target_Q = reward + ((1 - done) * discount * target_Q).detach()
+            # get the final target of the two Critic models, which is: Qt = r + γ * min(Qt1, Qt2), where γ is the discount factor
+            target_Q = reward + ((1 - done) * discount * target_Q).detach()
                 
-                # Step 10: The two Critic models take each the couple (s, a) as input and return two Q-values Q1(s,a) and Q2(s,a) as outputs
-                current_Q1, current_Q2 = self.critic(state, action)
+            # Step 10: The two Critic models take each the couple (s, a) as input and return two Q-values Q1(s,a) and Q2(s,a) as outputs
+            current_Q1, current_Q2 = self.critic(state, action)
                 
-                # Step 11: compute the loss coming from the two Critic models: Critic Loss = MSE_Loss(Q1(s,a), Qt) + MSE_Loss(Q2(s,a), Qt)
-                critic_loss = F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q)
+            # Step 11: compute the loss coming from the two Critic models: Critic Loss = MSE_Loss(Q1(s,a), Qt) + MSE_Loss(Q2(s,a), Qt)
+            critic_loss = F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q)
+            # Step 12: We backpropagate this Critic loss and update the parameters of the two Critic models with a SGD optimizer
+            self.critic_optimizer.zero_grad()
+            critic_loss.backward()
+            self.critic_optimizer.step()
+            # Step 13: Once every two iterations, we update our Actor model by performing gradient ascent on the output of the first Critic model
+            if it % policy_freq == 0:
+                actor_loss = -self.critic.Q1(state, self.actor(state)).mean()
+                self.actor_optimizer.zero_grad()
+                actor_loss.backward()
+                self.actor_optimizer.step()
+                # Step 14: Still once every two iterations, we update the weights of the Actor target by polyak averaging
+                for param, target_param in zip(self.critic.parameters(), self.critic_target.paramers()):
+                    target_param.data.copy_(tau * param.data + (1-tau) * target_param.data)
+                    # Step 15: Still once every two iterations, we update the weights of the Critic target by polyak averaging
+                    
+                for param, target_param in zip(self.actor.parameters(), self.critic_target.parameters()):
+                    target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
+    # Making a save method to save a trained model
+    def save(self, filename, directory):
+        torch.save(self.actor.state_dict(), '%s/%s_actor.pth' % (directory, filename))
+        torch.save(self.critic.state_dict(), '%s/%s_critic.pth' % (directory, filename))        
+    # Making a load method to load a pre-trained model
+    def load(self,filename, directory):
+        self.actor.load_state_dict(torch.load('%s/%s_actor.pth' % (directory, filename)))
+        self.critic.load_state_dict(torch.load('%s/%s_critic.pth' % (directory, filename)))
+        
+def evaluate_policy(policy, eval_episodes=10):
+    avg_reward = 0
+    for _ in range(eval_episodes):
+        obs = env.reset()
+        done = False
+        while not done:
+            action = policy.select_action(np.array(obs))
+            obs, reward, done, _ = env.step(action)
+            avg_reward += reward
+    avg_reward /= eval_episodes
+    print ("---------------------------------------")
+    print ("Average Reward over the Evaluation Step: %f" % (avg_reward))
+    print ("---------------------------------------")
+    return avg_reward
+
+env_name = 'Walker2DBulletEnv-v0'
+seed = 0 
+
+file_name = '%s_%s_%s' % ('TD3', env_name, str(seed))
+print ("---------------------------------------")
+print ("Settings: %s" % (file_name))
+print ("---------------------------------------")
+
+eval_episodes = 10
+save_env_vid = True
+env = gym.make(env_name)
+max_episodes_steps = env._max_episode_steps
+if save_env_vid:
+    env = wrappers.Moniter(env, monitor_dir, force = True)
+    env.reset()
+env.seed(seed)
+torch.manual_seed(seed)
+np.random.seed(seed)
+state_dim = env.action_space.shape[0]
+max_action = float(env.action_space.high[0])
+policy = TD3(state_dim, action_dim, max_action)
+policy.load(file_name, './pytorch_models/')
+_ = evaluate_policy(policy, eval_episodes=eval_episodes)
+                        
+                                        
+                     
+                 
         
         
